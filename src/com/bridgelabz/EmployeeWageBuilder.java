d43ab89746d6f17bf8a48fe147d0eb37bb94e2b5
Rohan Jadhav <rohanj121999@gmail.com>
2021-08-03 19:22:50 -0700

    Added:Employee Attendence Checking

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,5 +3,13 @@ package com.bridgelabz;
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Builder Computation");
+
+        int IS_PRESENT = 1;
+
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println(" Employee is Present");
+        else
+            System.out.println(" Employee is Absent");
     }
 }
